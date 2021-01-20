1c9e5336ff23539bbd65629dd89b6f0ff0859b8c
almustafago <almustafago@gmail.com>
2021-01-20 23:21:09 +0300

    New Update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -57,7 +57,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">
+          {{ Auth::user()->name }}
+          </a>
         </div>
       </div>
 
